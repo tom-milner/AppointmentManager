958055fd4951fd589fd6eed6442237556247ac63
tom-milner <tom.f.milner@gmail.com>
2019-07-31 10:56:45 +0100

    updated appointmentModel

--- src/models/AppointmentModel.js
+++ src/models/AppointmentModel.js
@@ -3,22 +3,50 @@ const Schema = mongoose.Schema;
 
 // define schema
 let appointmentSchema = new Schema({
-    // date and time of appointment
-    date: {
-        type: Date,
-        required: true
-    },
+
     // title of appointment
     title: {
         type: String,
         require: true,
         max: 100
     },
-    // client that bookec the appointment
-    client: {
+    // clients that booked the appointment
+    clients: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
+    }],
+    // location of appointment
+    loc: {
+        type: {
+            type: String,
+            required: true
+        },
+        coordinates: {
+            type: [Number], // MUST BE LONGITUDE, THEN LATITUDE
+            required: true,
+        }
     },
+    // allocated timeslots for the appointment
+    timeslots: [{
+        startTime: {
+            type: Date,
+            required: true
+        },
+        endTime: {
+            type: Date,
+            required: true
+        }
+    }],
+    // whether the apointment has been confirmed or not
+    isApproved: {
+        type: Boolean,
+        required: true
+    }
+});
+
+// tie an index to the schema so that it can be used to search
+appointmentSchema.index({
+    "loc": "2dsphere"
 });
 
 
