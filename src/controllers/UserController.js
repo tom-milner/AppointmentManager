acccd9194c1736dbf91735039d4d46d4360b716b
tom-milner <tom.f.milner@gmail.com>
2019-08-13 21:22:14 +0100

    added getAllCounsellors function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,35 +1,71 @@
-let UserModel = require("../models/UserModel");
-let AppointmentModel = require("../models/AppointmentModel");
-const moment = require("moment");
-
+const UserModel = require("../models/UserModel");
+const Role = require("../models/Role");
 // TODO: Build UserControllerPolicy
 
 // takes array of client Ids an returns simple user object.
 async function getUsernamesFromUserIds(req, res) {
   // clientIds is a string with comma seperated values
-
-  let userIds = (req.query.userIds).split(",").filter(Boolean);
-  console.log(userIds);
-  let users = [];
-  for (id of userIds) {
-    let user = await UserModel.findOne({
-      _id: id
-    });
-    if (user) {
-      user.password = undefined;
-      users.push(user);
+  try {
+    let userIds = (req.query.userIds).split(",").filter(Boolean);
+    let users = [];
+    for (id of userIds) {
+      let user = await UserModel.findOne({
+        _id: id
+      });
+      if (user) {
+        user.password = undefined;
+        users.push(user);
+      }
     }
+    res.status(200).send({
+      success: true,
+      message: "Users returned successfully",
+      users: users
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      message: "Error getting usernames."
+    })
   }
 
+}
 
-  res.status(200).send({
-    success: true,
-    message: "Users returned successfully",
-    users: users
-  });
+async function getAllCounsellors(req, res) {
 
+  try {
+    let counsellors = await UserModel.find({
+      role: Role.Counsellor
+    });
+
+    // make sure counsellors could be found
+    if (!counsellors) {
+      throw {
+        message: "No counsellors could be found.",
+        code: 404
+      }
+    }
+
+    // exclude unnecessary info.
+    counsellors.forEach(counsellor => {
+      counsellor.password = counsellor.email = undefined;
+    });
+
+    res.status(200).send({
+      success: true,
+      message: "Counsellors returned successfully",
+      counsellors: counsellors
+    })
+
+  } catch (error) {
+    res.status(error.code || 500).send({
+      success: false,
+      message: error.message || "Error returning counsellors.",
+    })
+  }
 }
 
 module.exports = {
   getUsernamesFromUserIds,
+  getAllCounsellors
 }
