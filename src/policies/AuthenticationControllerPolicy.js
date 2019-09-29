2c1c3f69853e156b8186aa5b977ac6d5a3930f99
tom-milner <tom.f.milner@gmail.com>
2019-09-29 17:42:49 +0100

    changed error message

--- src/policies/AuthenticationControllerPolicy.js
+++ src/policies/AuthenticationControllerPolicy.js
@@ -28,12 +28,8 @@ function register(req, res, next) {
         errorMessage = "You must provide a valid email address";
         break;
       case "password":
-        errorMessage = `The password provided failed to match the following rules: 
-                        <br>
-                        1. It must contain ONLY the following characters: lower case, upper case, numerics, and punctuation.
-                        <br>
-                        2. It must be at least 8 characters in length and not greater than 32 characters in length.
-                        `
+        errorMessage =
+          "Password must be at least 8 characters in length and not greater than 32 characters in length."
         errorCode = 400;
         break;
 
