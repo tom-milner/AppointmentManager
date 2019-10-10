8332af07476381f91dc3c3d7a7dd1fd174eced0f
tom-milner <tom.f.milner@gmail.com>
2019-10-10 17:22:00 +0100

    Added automatic guest account deletion

--- src/config/scheduler/Scheduler.js
+++ src/config/scheduler/Scheduler.js
@@ -2,7 +2,8 @@
 const cron = require('node-cron');
 const PasswordResetModel = require("../../models/MongooseModels/PasswordResetModel");
 const moment = require("moment");
-
+const GuestModel = require("../../models/MongooseModels/UserModels/GuestModel");
+const AppointmentModel = require("../../models/MongooseModels/AppointmentModel");
 
 function start() {
 
@@ -22,7 +23,7 @@ function start() {
   });
 
   // remove client password resets (these are allowed to stay for longer as they are used to activate client accounts.)
-  cron.schedule("0 0 * * 1", async () => {
+  cron.schedule("0 0 * * *", async () => {
     let expiryTime = moment().subtract(1, "month");
     let deletedResets = await PasswordResetModel.deleteMany({
       timestamp: {
@@ -32,6 +33,36 @@ function start() {
     });
     console.log(`Removed ${deletedResets.deletedCount}/${deletedResets.n} client password resets.`)
   })
+
+  // remove guest accounts no longer in use.
+  cron.schedule("* * * * *", async () => {
+    let allGuests = await GuestModel.find({});
+    let oneWeekAgo = moment().subtract(1, "week");
+
+    let deletedCount = 0;
+    let totalGuests = allGuests.length;
+
+    for (let guest of allGuests) {
+      let involvedAppointments = await AppointmentModel.find({
+        clients: guest._id,
+        startTime: {
+          $gt: oneWeekAgo
+        }
+      });
+
+      if (!involvedAppointments || involvedAppointments.length < 1) {
+
+        if (moment(guest.timeCreated).isBefore(oneWeekAgo)) {
+          // delete the guest account
+          await GuestModel.findByIdAndDelete(guest._id);
+          deletedCount++;
+        }
+      }
+
+    }
+
+    console.log(`Deleted ${deletedCount}/${totalGuests} guest accounts. `)
+  });
 
 }
 
