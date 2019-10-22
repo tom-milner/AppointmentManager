4e1b9438195c916da1b315569d7312812483d277
tom-milner <tom.f.milner@gmail.com>
2019-10-22 18:56:23 +0100

    Minor changes

--- api/src/struct/AppResponse.js
+++ api/src/struct/AppResponse.js
@@ -1,47 +1,51 @@
 class AppResponse {
 
-  constructor(res) {
-    this.response = res;
-  }
-
-  success(message, payload, statusCode) {
-
-    let responseObject = {
-      success: true,
-      message: message
+    constructor(res) {
+        this.response = res;
     }
 
-    if (payload) {
-      // add payload to errorObject
-      let keys = Object.keys(payload);
-      for (let key of keys) {
-        if (key == "success" || key == "message") break;
-        responseObject[key] = payload[key]
-      }
-    }
-    this.response.status(statusCode || 200).send(responseObject);
-  }
+    success(message, payload, statusCode) {
 
-  failure(errorMessage, statusCode, payload) {
+        let responseObject = {
+            success: true,
+            message: message
+        }
 
-    // create error object
-    let errorObject = {
-      success: false,
-      message: errorMessage || "Server error.",
-    }
-
-    if (payload) {
-      // add payload to errorObject
-      let keys = Object.keys(payload);
-      for (let key of keys) {
-        if (key == "success" || key == "message") break;
-        errorObject[key] = payload[key]
-      }
+        if (payload) {
+            // add payload to errorObject
+            let keys = Object.keys(payload);
+            for (let key of keys) {
+                if (key == "success" || key == "message") break;
+                responseObject[key] = payload[key]
+            }
+        }
+        this.response.status(statusCode || 200).send(responseObject);
     }
 
-    // send error 
-    this.response.status(statusCode).send(errorObject)
-  }
+    failure(errorMessage, statusCode, payload) {
+
+        if (statusCode < 100 || statusCode > 598) {
+            console.log("Invalid status code!");
+        }
+
+        // create error object
+        let errorObject = {
+            success: false,
+            message: errorMessage || "Server error.",
+        }
+
+        if (payload) {
+            // add payload to errorObject
+            let keys = Object.keys(payload);
+            for (let key of keys) {
+                if (key == "success" || key == "message") break;
+                errorObject[key] = payload[key]
+            }
+        }
+
+        // send error 
+        this.response.status(statusCode).send(errorObject)
+    }
 
 }
 
