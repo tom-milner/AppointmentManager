26fdeecd92dd278944b4081a125d8ba78e4b779d
tom-milner <tom.f.milner@gmail.com>
2019-10-22 17:55:01 +0100

    ecosystem changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
         name: "api",
-        cwd: "/home/ubuntu/Projects/AppointmentManager/api/",
+        cwd: "/home/ubuntu/Projects/AppointmentManager/api/src",
         script: "app.js",
         env: {
             "NODE_ENV": "production"
