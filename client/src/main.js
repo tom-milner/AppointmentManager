56757d2ebb2baf842ede9096d04cee34d87460a2
tom-milner <tom.f.milner@gmail.com>
2019-11-01 10:37:56 +0000

    Added sentry to client

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,18 @@ import router from "./routes";
 import store from "./store/store";
 import AuthenticationService from "@/services/AuthenticationService";
 
+// Setup error monitoring
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+Sentry.init({
+    dsn: 'https://56454897973441c8a6a4d93b7286e72f@sentry.io/1804724',
+    integrations: [new Integrations.Vue({
+        Vue,
+        attachProps: true
+    })]
+});
+
+
 
 // setup moment for date time formatting
 import moment from "moment";
@@ -19,7 +31,7 @@ AuthenticationService.initializeNavigationGuard();
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
